eb434e7e23798d9a874bd1ccdb811838e5e597fa
ryanmunger <ryanmunger@gmail.com>
2014-06-24 22:52:20 -0700

    Add new relic

---
+++ newrelic.js
@@ -0,0 +1,24 @@
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name : ['cambrian-site'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key : '5057453313411255782e9661fc1f8fc5c5585a2d',
+  logging : {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level : 'info'
+  }
+};
